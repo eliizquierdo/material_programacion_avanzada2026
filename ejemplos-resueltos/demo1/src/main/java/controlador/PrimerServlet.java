ae374cd17df21bb325b0902f4bdaf307bf6277d7
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2026-07-18 10:26:49 -0300

    Update PrimerServlet.java

--- ejemplos-resueltos/demo1/src/main/java/controlador/PrimerServlet.java
+++ ejemplos-resueltos/demo1/src/main/java/controlador/PrimerServlet.java
@@ -1,11 +1,11 @@
 package controlador;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/Servlet1")
 public class PrimerServlet extends HttpServlet {
