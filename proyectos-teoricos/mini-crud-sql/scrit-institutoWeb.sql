1a4c43db7b7bd5875d7f604a0aa67fbafaf18076
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2026-07-20 17:33:09 -0300

    Fix table name from 'persona' to 'personas'

--- proyectos-teoricos/mini-crud-sql/scrit-institutoWeb.sql
+++ proyectos-teoricos/mini-crud-sql/scrit-institutoWeb.sql
@@ -3,7 +3,7 @@ CREATE database institutoWeb;
 
 USE institutoWeb;
 
-CREATE TABLE persona (
+CREATE TABLE personas (
     codigo INT(8),
     nombre VARCHAR(10) NOT NULL,
     PRIMARY KEY (codigo)
@@ -11,7 +11,7 @@ CREATE TABLE persona (
 
 -- Insertamos algunos datos de prueba
 INSERT INTO
-    persona (codigo, nombre)
+    personas (codigo, nombre)
 VALUES (1, 'Juan'),
     (2, 'María'),
     (3, 'Carlos');
