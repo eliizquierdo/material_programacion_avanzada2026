a24ab255ba7ac243858d57c476d24a821ce73ab3
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2026-07-20 17:33:26 -0300

    Rename table from 'persona' to 'personas'

--- .devcontainer/mini-crud-sql/db/init.sql
+++ .devcontainer/mini-crud-sql/db/init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS persona (
+CREATE TABLE IF NOT EXISTS personas (
     codigo INT(8),
     nombre VARCHAR(10) NOT NULL,
     PRIMARY KEY (codigo)
 );
 
 INSERT IGNORE INTO
-    persona (codigo, nombre)
+    personas (codigo, nombre)
 VALUES (1, 'Juan'),
     (2, 'María'),
     (3, 'Carlos');
